6ce9cea7e2ebb221540357e56f87e9344fdc08b7
JAIOMP <jaiomp@thoughtworks.com>
2016-11-18 22:07:56 +0530

    Exercise 14 completed

---
+++ Exercise17/main.js
@@ -0,0 +1,32 @@
+var dataset = [1, 1, 2, 2, 1, 2, 1];
+
+const RADIUS = 150;
+const WIDTH = 600;
+const HEIGHT = 400;
+var color = ["#1F77B4","#AE87E8","#AEC7E8","#FFBB78","#2CA02C","#98DF8A","#D62728"];
+var arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(RADIUS);
+
+
+var svg = d3.select('.container')
+    .append('svg')
+    .attr('width', WIDTH)
+    .attr('height', HEIGHT)
+    .append('g')
+    .attr('transform', 'translate(' + (WIDTH / 2) +  ',' + (HEIGHT / 2) + ')');
+
+var pie = d3.pie()
+    .value(function(d) { return d})
+    .sort(null)
+    .startAngle((Math.PI/180))
+    .endAngle(180*(Math.PI/180));
+
+var path = svg.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr("d", arc)
+    .attr('fill', function(d,i){
+        return color[i];
+    });
